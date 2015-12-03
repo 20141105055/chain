dbc2ff51c5ddb98d9cbd86d2e66e5465bf35d7f9
卢佳明 <571836353@qq.com>
2015-12-03 10:32:21 +0800

    Initial Commit

---
+++ chain/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chain
+//
+//  Created by 20141105055ljm on 15/12/3.
+//  Copyright (c) 2015年 20141105055ljm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
